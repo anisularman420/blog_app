953c1c1469d7a282be2080b78cb7e9afb838f594
anisularman420 <Anisul.Arman19@spsmail.cuny.edu>
2023-12-17 22:07:58 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'your_secret_key'# Disable Flask-SQLAlchemy modification tracking
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)  # Initialize Flask-Migrate
+login_manager = LoginManager(app)
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    published_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    author = db.Column(db.String(80), nullable=False)
+    is_published = db.Column(db.Boolean, default=True)
+    category = db.Column(db.String(50))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/')
+def index():
+    posts = Post.query.filter_by(is_published=True).order_by(Post.published_date.desc()).all()
+    return render_template('index.html', posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            login_user(user)
+            return redirect(url_for('dashboard'))
+
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    user_posts = Post.query.filter_by(author=current_user.username).all()
+    return render_template('dashboard.html', posts=user_posts)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        migrate.init_app(app)  # Initialize Flask-Migrate
+    app.run(debug=True)
