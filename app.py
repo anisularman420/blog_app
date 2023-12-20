c4b35782c444c6c2438506379effaff7eeb2e938
anisularman420 <Anisul.Arman19@spsmail.cuny.edu>
2023-12-20 12:05:13 -0500

    front-end of blog, added edit and delete for post and routes

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
+# Import necessary modules
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from datetime import datetime
+from flask import render_template, request, flash
+from werkzeug.security import check_password_hash
 
+
+# Create Flask app and configure it
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'your_secret_key'# Disable Flask-SQLAlchemy modification tracking
+app.config['SECRET_KEY'] = 'your_secret_key'  # Disable Flask-SQLAlchemy modification tracking
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)  # Initialize Flask-Migrate
 login_manager = LoginManager(app)
 
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -26,10 +28,26 @@ class Post(db.Model):
     is_published = db.Column(db.Boolean, default=True)
     category = db.Column(db.String(50))
 
+
+# Define User and Post models
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(120), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+# Configure login manager to load user
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+# Define routes
 @app.route('/')
 def index():
     posts = Post.query.filter_by(is_published=True).order_by(Post.published_date.desc()).all()
@@ -42,7 +60,7 @@ def login():
         password = request.form.get('password')
         user = User.query.filter_by(username=username).first()
 
-        if user and user.password == password:
+        if user and user.check_password(password):
             login_user(user)
             return redirect(url_for('dashboard'))
 
@@ -54,14 +72,79 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
-@app.route('/dashboard')
+@app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
     user_posts = Post.query.filter_by(author=current_user.username).all()
     return render_template('dashboard.html', posts=user_posts)
 
+@app.route('/edit_post/<int:post_id>', methods=['GET', 'POST'])
+@login_required
+def edit_post(post_id):
+    post = Post.query.get_or_404(post_id)
+
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.content = request.form['content']
+        db.session.commit()
+        return redirect(url_for('dashboard'))
+
+    return render_template('edit_post.html', post=post, action='Edit', form_action=url_for('edit_post', post_id=post.id))
+
+@app.route('/delete_post/<int:post_id>', methods=['GET', 'POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+
+    if request.method == 'POST':
+        db.session.delete(post)
+        db.session.commit()
+        return redirect(url_for('dashboard'))
+
+    return render_template('delete_post.html', post=post)
+
+@app.route('/add_post', methods=['GET', 'POST'])
+@login_required
+def add_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        new_post = Post(title=title, content=content, author=current_user.username)
+        db.session.add(new_post)
+        db.session.commit()
+
+        return redirect(url_for('dashboard'))
+
+    return render_template('add_post.html')
+from flask import render_template, request, flash
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        # Validate the form data (you should add more validation)
+        if not username or not password:
+            flash('Please enter both username and password', 'error')
+            return redirect(url_for('register'))
+
+        # Create a new user and add it to the database
+        new_user = User(username=username)
+        new_user.set_password(password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('Registration successful! Please log in.', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+
+# Run the application
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        migrate.init_app(app)  # Initialize Flask-Migrate
+        migrate.init_app(app)
     app.run(debug=True)
